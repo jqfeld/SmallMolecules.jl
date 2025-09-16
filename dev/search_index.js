8d758c558a6e3c06badde469803a7a4d9bd2e1d2
Documenter.jl <documenter@juliadocs.github.io>
2025-09-16 23:26:54 +0000

    build based on bffcd24

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SmallMolecules","page":"Home","title":"SmallMolecules","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SmallMolecules.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
